b9c785eb5e957ce9dbf5581417a41418c52df1a8
Kadoby <arnaud.binet.44@gmail.com>
2023-05-23 09:47:37 +0200

    Récupération de la caméra

--- Wifibot/mainwindow.cpp
+++ Wifibot/mainwindow.cpp
@@ -1,15 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QUrl>
+#include <QWebEngineView>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+MainWindow::MainWindow(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+
+    ui->setupUi(this);
+
+
+    //Vue de la webcam du robot
+    manager = new QNetworkAccessManager;
+    view = new QWebEngineView();
+    view->load(QUrl("http://192.168.1.106:8080/?action=stream"));
+    view->show();
+
+    this->ui->camera->addWidget(view);
+
+    robot = new MyRobot(this);
+
+
+}
